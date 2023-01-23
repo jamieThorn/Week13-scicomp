566cb080fee5b46a3b37d4df21e9b58c02e70d21
jamieThorn <xu20901@bristol.ac.uk>
2023-01-23 12:36:05 +0000

    finished ex1

--- Exercise1.py
+++ Exercise1.py
@@ -1,10 +1,25 @@
+from math import pi
 def leibniz(N):
     pi_N = 0
     for n in range(N):
         nth = 8/((4*n+1)*(4*n+3))
         pi_N += nth    
     return pi_N
-
+def error(pi_N):
+    e = abs(pi-pi_N)
+    return e
+def test():
+    n = 10000
+    while error(leibniz(n)) > 10**(-7):
+        n+=1
+    return n
 def main():
-    print(leibniz(1000))
+    pi_100 = leibniz(100)
+    pi_1000 = leibniz(1000)
+    pi_10000 = leibniz(10000)
+    print(error(pi_100))
+    print(error(pi_1000))
+    print(error(pi_10000))
+    nth = test()
+    print(nth)
 main()
